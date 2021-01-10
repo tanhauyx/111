b9475bb5ad26c3c2ec1fb1985d7ae63d042bd442
lufeifei <1251200096@qq.com>
2021-01-10 16:31:23 +0800

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student  extends user {
+    private  String major;
+    private String phone;
+
+    public String getMajor(){ return major;}
+
+    public void setMajor(String major) {  this.major = major; }
+
+    public String getPhone() { return phone; }
+
+    public void setPhone(String phone){ this.phone=phone; }
+
+}
